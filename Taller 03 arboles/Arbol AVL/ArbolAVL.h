d4b1579cd3a0345d47fd018765e02dbd9aace51f
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2024-11-12 15:39:37 -0500

    Add files via upload

---
+++ Taller 03 arboles/Arbol AVL/ArbolAVL.h
@@ -0,0 +1,38 @@
+#ifndef ARBOLAVL_H_INCLUDED
+#define ARBOLAVL_H_INCLUDED
+
+#include "NodoBinario.h"
+
+template < class T >
+class ArbolAVL {
+protected:
+    NodoBinario<T>* raiz;
+public:
+    ArbolAVL();
+    ~ArbolAVL();
+    bool esVacio();
+    T datoRaiz();
+    bool insertarRaiz(NodoBinario<T>* val);
+    int altura();
+    int altura(NodoBinario<T>* nodo);
+    int tamano();
+    bool buscar(T val);
+    void preOrden();
+    void inOrden();
+    void posOrden();
+    void nivelOrden();
+    NodoBinario<T>* obtenerSucesor(NodoBinario<T>* nodo);
+    int balanceo(NodoBinario<T>* nodo);
+    NodoBinario<T>* balancear(NodoBinario<T>* nodo);
+    NodoBinario<T>* insertarRec(NodoBinario<T>* nodo, T val);
+    void insertar(T val);
+    NodoBinario<T>* eliminarRec(NodoBinario<T>* nodo, T val);
+    NodoBinario<T>* encontrarMinimo(NodoBinario<T>* nodo);
+    void eliminar(T val);
+    NodoBinario<T>* rotacionDerecha(NodoBinario<T>* y);
+    NodoBinario<T>* rotacionIzquierda(NodoBinario<T>* x);
+};
+
+#include "ArbolAVL.hxx"
+
+#endif // ARBOLAVL_H_INCLUDED
---
+++ Taller 03 arboles/Arbol AVL/NodoBinario.h
@@ -0,0 +1,30 @@
+#ifndef NODOBINARIO_H_INCLUDED
+#define NODOBINARIO_H_INCLUDED
+
+template < class T >
+class NodoBinario {
+protected:
+    T dato;
+    NodoBinario<T>* hijoIzq;
+    NodoBinario<T>* hijoDer;
+public:
+    NodoBinario();
+    NodoBinario(T val);
+    ~NodoBinario();
+    T obtenerDato();
+    void fijarDato(T val);
+    NodoBinario<T>* obtenerHijoIzq();
+    NodoBinario<T>* obtenerHijoDer();
+    void fijarHijoIzq(NodoBinario<T>* izq);
+    void fijarHijoDer(NodoBinario<T>* der);
+    bool esHoja();
+    int altura();
+    void inOrden();
+    void preOrden();
+    void posOrden();
+    int tamano();
+};
+
+#include "NodoBinario.hxx"
+
+#endif // NODOBINARIO_H_INCLUDED
